3bfce9c20b6d6d361514f05881480ea9c6e686a7
Unknown <simon.baehler@heig-vd.ch>
2017-07-07 16:29:22 +0200

    Listener -> handler

--- Assets/scripts/KeywordHandler.cs
+++ Assets/scripts/KeywordHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using HoloToolkit.Unity.InputModule;
 using UnityEngine;
 
-public class KeywordListener : MonoBehaviour
+public class KeywordHandler : MonoBehaviour
 {
 	private GazeManager gm;
 	// Use this for initialization
@@ -16,6 +16,15 @@ public class KeywordListener : MonoBehaviour
 		{
 			//gm.HitObject.GetComponent<Civilian>().OnPolice();
 			gm.HitObject.GetComponent<Civilian>().SendMessage("OnPolice");
+		}
+	}
+
+	public void OnSayStop()
+	{
+		if (gm.HitObject != null && gm.HitObject.GetComponent<ShopShooter>() != null)
+		{
+			//gm.HitObject.GetComponent<Civilian>().OnPolice();
+			gm.HitObject.GetComponent<ShopShooter>().SendMessage("OnStop");
 		}
 	}
 }
